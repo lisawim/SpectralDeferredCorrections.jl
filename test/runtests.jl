b14b9366a65522badc8aa847a6be42c8e0b9c6a6
lisawim <wimmer@uni-wuppertal.de>
2024-12-08 11:44:03 +0100

    Updated test file

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,6 @@ using Coverage
 using SpectralDeferredCorrections
 using Test
 
-process_folder()  # Generates the lcov.info file
-
-using Test
-using SpectralDeferredCorrections  # Use the module
-
 @testset "Testing SpectralDeferredCorrections" begin
     @test add_numbers(2, 3) == 5
     @test add_numbers(-1, 1) == 0
