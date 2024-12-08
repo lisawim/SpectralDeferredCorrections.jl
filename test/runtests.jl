b1a16a3ee80fd736566478b2816af28bea53cd72
lisawim <wimmer@uni-wuppertal.de>
2024-12-08 11:50:30 +0100

    Update everything again

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,3 +6,7 @@ using Test
     @test add_numbers(2, 3) == 5
     @test add_numbers(-1, 1) == 0
 end
+
+# Generate coverage report
+Coverage.process_folder("src")
+Coverage.lcov("src", "lcov.info")  # Creates lcov.info file
