8f6f60ff747d3a2fb9aed6e8188f7d5c3ac21899
lisawim <wimmer@uni-wuppertal.de>
2024-12-21 16:43:06 +0100

    Added abstract type for simulator.

--- src/SpectralDeferredCorrections.jl
+++ src/SpectralDeferredCorrections.jl
@@ -4,6 +4,7 @@ using LinearAlgebra
 
 # Include core modules
 include(joinpath(@__DIR__, "core/errors.jl"))
+include(joinpath(@__DIR__, "core/simulation.jl"))
 include(joinpath(@__DIR__, "core/problem_ode.jl"))
 
 # Include problem files
@@ -14,6 +15,7 @@ include(joinpath(@__DIR__, "sweepers/inner_solvers.jl"))
 
 using .ProblemODEBase
 using .Errors
+using .SimulationBase
 using .LinearTestEquation
 using .InnerSolvers
 
@@ -21,5 +23,7 @@ export AbstractProblemODE
 export LinearTestSPP, f, solve, u_exact
 export ConvergenceError, NotImplementedError
 export newton
+
+export AbstractSimulator, run_simulation
 
 end
---
+++ src/core/simulation.jl
@@ -0,0 +1,14 @@
+module SimulationBase
+
+using ..Errors
+
+export AbstractSimulator, run_simulation
+
+
+abstract type AbstractSimulator end
+
+function run_simulation(simulator::AbstractSimulator)
+    throw(NotImplementedError("run_simulation function is not implemented!"))
+end
+    
+end
