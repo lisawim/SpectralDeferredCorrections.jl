bc2c6aad5008f5e5398bb0bbd97e1b1e2e56564c
lisawim <wimmer@uni-wuppertal.de>
2024-12-22 16:04:18 +0100

    Base to initialize the collocation stuff.

--- src/core/collocation.jl
+++ src/core/collocation.jl
@@ -15,12 +15,17 @@ struct Collocation
     nodes::Vector{Float64}
     weights::Vector{Float64}
     Q::Matrix{Float64}
+    QD::String
 end
 
-function Collocation(num_nodes::Integer = nothing, node_type::String = "LEGENDRE",
-        quad_type::String = "RADAU-RIGHT", t_left::Float64 = 0.0, t_right::Float64 = 1.0)
+function Collocation(num_nodes::Integer = nothing, quad_type::String = "RADAU-RIGHT",
+        node_type::String = "LEGENDRE", QD::String = nothing, t_left::Float64 = 0.0, t_right::Float64 = 1.0)
     if isnothing(num_nodes)
         throw(Parameter("Number of collocation nodes num_nodes have to be passed!"))
+    end
+
+    if isnothing(QD)
+        throw(ParameterError("Type of QDelta matrix must be provided!"))
     end
 
     # Generator for collocation
@@ -31,7 +36,22 @@ function Collocation(num_nodes::Integer = nothing, node_type::String = "LEGENDRE
     nodes = gen.nodes
     weights = gen.weights
     Q = gen.Q
-    return Collocation(num_nodes, node_type, quad_type, t_left, t_right, nodes, weights, Q)
+    return Collocation(
+        num_nodes, node_type, quad_type, t_left, t_right, nodes, weights, Q, QD)
+end
+
+function get_implicit_Qdelta(collocation::Collocation, QI::String, k::Int = nothing)
+    QD_gen = qmat.QDELTA_GENERATORS[collocation.QD]
+    gen = QD_gen(Q = collocation.gen.Q, nNodes = collocation.num_nodes,
+        nodeType = collocation.node_type, quadType = collocation.quad_type,
+        nodes = collocation.nodes, tLeft = collocation.tleft
+    )
+
+    if isnothing(k)
+        return gen.genCoeff(QI)
+    else
+        return gen.genCoeff(QI, k = k)
+    end
 end
 
 end
